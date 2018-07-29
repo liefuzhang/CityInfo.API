b8e36a9c32e6a5387900cf9489cd8974951476bf
liefu zhang <liefuzhangnz@gmail.com>
2018-07-29 12:33:01 +1200

    Adding mvc and cities controller

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<Object> {
+                new {id=1, Name="New York City"},
+                new {id=2, Name="Paris"}
+            });
+        }
+    }
+}
